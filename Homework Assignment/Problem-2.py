221c61f271c1107929458a5a818d8a08811b6499
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-14 21:30:53 +0530

    Update Problem-2.py

--- Homework Assignment/Problem-2.py
+++ Homework Assignment/Problem-2.py
@@ -1,12 +1,3 @@
-def get_min_max(arr):
-    minimum = maximum = arr[0]
-    for x in arr:
-        if x < minimum:
-            minimum = x
-        if x > maximum:
-            maximum = x
-    return minimum, maximum
-
-# Example
 arr = [1, 4, 3, 5, 8, 6]
-print(get_min_max(arr))
+print(min(arr), max(arr))
+
