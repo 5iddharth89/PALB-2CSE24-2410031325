32fa8beb26056dd54f3cc52d5ecea2377ebf87e8
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-14 21:44:35 +0530

    Create Problem-7.py

---
+++ Homework Assignment/Problem-7.py
@@ -0,0 +1,9 @@
+arr = [1, 2, 3, -2, 5]
+max_sum = arr[0]
+curr_sum = arr[0]
+
+for i in range(1, len(arr)):
+    curr_sum = max(arr[i], curr_sum + arr[i])
+    max_sum = max(max_sum, curr_sum)
+
+print(max_sum)
