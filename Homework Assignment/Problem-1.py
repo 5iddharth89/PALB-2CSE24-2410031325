ea0c33573273381378cba3a7ea60d685f4047e8b
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-14 21:24:01 +0530

    Update Problem-1.py

--- Homework Assignment/Problem-1.py
+++ Homework Assignment/Problem-1.py
@@ -1,5 +1,11 @@
-# Problem 1: Reverse an array
+def reverse_array(arr):
+    i, j = 0, len(arr) - 1
+    while i < j:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+    return arr
 
+# Example
 arr = [1, 4, 3, 2, 6, 5]
-arr.reverse()
-print(arr)
+print(reverse_array(arr))
