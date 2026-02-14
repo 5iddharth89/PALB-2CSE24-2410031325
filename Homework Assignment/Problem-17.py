ddc04d42eb87ec09267c72d4291516b7d5df6547
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-14 22:45:21 +0530

    Create Problem-17.py

---
+++ Homework Assignment/Problem-17.py
@@ -0,0 +1,24 @@
+def commonElements(a, b, c):
+    i = j = k = 0
+    result = []
+    
+    while i < len(a) and j < len(b) and k < len(c):
+        if a[i] == b[j] == c[k]:
+            if not result or result[-1] != a[i]:
+                result.append(a[i])
+            i += 1
+            j += 1
+            k += 1
+        elif a[i] < b[j]:
+            i += 1
+        elif b[j] < c[k]:
+            j += 1
+        else:
+            k += 1
+    
+    return result if result else [-1]
+
+# Example
+print(commonElements([1,5,10,20,40,80],
+                     [6,7,20,80,100],
+                     [3,4,15,20,30,70,80,120]))
