00ef0fe88fee2624b55c3f1fbac4920684df04d5
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-27 18:20:08 +0530

    Create problem-35.py

---
+++ Homework Assignment/problem-35.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    d = defaultdict(list)
+
+    for word in strs:
+        key = ''.join(sorted(word))
+        d[key].append(word)
+
+    return list(d.values())
+
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
