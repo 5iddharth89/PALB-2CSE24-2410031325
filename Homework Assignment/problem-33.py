6c362928888712be991aabc0db29ae4eba70c89e
5iddharth89 <siddharth.jindal.cs28@iilm.edu>
2026-02-27 18:18:03 +0530

    Create problem-33.py

---
+++ Homework Assignment/problem-33.py
@@ -0,0 +1,21 @@
+def combinationSum2(candidates, target):
+    candidates.sort()
+    res = []
+
+    def backtrack(start, path, total):
+        if total == target:
+            res.append(path[:])
+            return
+
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i-1]:
+                continue
+            if total + candidates[i] > target:
+                break
+            backtrack(i+1, path + [candidates[i]], total + candidates[i])
+
+    backtrack(0, [], 0)
+    return res
+
+
+print(combinationSum2([10,1,2,7,6,1,5], 8))
